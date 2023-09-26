7aaece4b402ce552a17c0a1b6cc8dec25605c1f1
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-09-26 07:29:23 -0700

    Updated app.py with new features

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json  # Assuming the incoming request has JSON payload
-    print("Received data:", data)  # Print the data to the console
-
-    # Your existing code to create DataFrame and other operations
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    # Get JSON data from the incoming request
+    data = request.json
     df = pd.DataFrame(data)
-    # ... rest of your code
-
-    return jsonify({'average_age': average_age})
+    
+    # Perform a simple Pandas operation
+    # Multiply the 'Age' column by 2 and store it in a new column 'Double_Age'
+    df['Double_Age'] = df['Age'] * 2
+    
+    # Convert the modified DataFrame back to JSON
+    result = df.to_json(orient='split')
+    
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port=5000)
-
 
