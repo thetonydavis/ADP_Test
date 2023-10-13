c4b3756dc20b2271eefb75aaa2a606fe24744a1c
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-13 10:07:07 -0700

    Update app.py
    added error code

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello, World!"
+
 @app.route('/webhook', methods=['POST'])
 def process_data():
     data = request.json  # Assuming data is received as JSON
@@ -12,19 +16,18 @@ def process_data():
         # Create an index if data consists of scalar values
         index = range(len(data)) if data else None
         df = pd.DataFrame(data, index=index)
-    
+
     # Check if 'Age' column exists
     if 'Age' in df.columns:
         # Perform a simple Pandas operation
         df['NewAge'] = df['Age'] * 2
     else:
         return jsonify({"error": "'Age' column not found in data"}), 400
-    
+
     # Convert the modified DataFrame back to JSON
     result = df.to_json(orient='split')
-    
+
     return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
