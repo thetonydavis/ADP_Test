0797e01636d02ecec1ea0ab6f35882436cdaabca
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-13 15:04:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,28 +15,35 @@ app = Flask(__name__)
 def process_data():
     data = request.json  # Assuming data is received as JSON
     try:
-        df = pd.DataFrame(data)
+        incoming_df = pd.DataFrame(data)
     except ValueError:
         index = range(len(data)) if data else None
-        df = pd.DataFrame(data, index=index)
+        incoming_df = pd.DataFrame(data, index=index)
 
-    if 'Age' in df.columns:
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials_json = os.environ['GOOGLE_CREDENTIALS_JSON']
+    credentials_info = json.loads(credentials_json)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
+
+    client = gspread.authorize(creds)
+    sheet = client.open("ADP Test Sheet").sheet1
+
+    # Fetch all records from the Google Sheet
+    records = sheet.get_all_records()
+    df = pd.DataFrame.from_records(records)
+
+    if 'Age' in incoming_df.columns:
         # Convert 'Age' to integers and then perform the multiplication
-        df['Age'] = df['Age'].astype(int)
-        df['NewAge'] = df['Age'] * 2
+        incoming_df['Age'] = incoming_df['Age'].astype(int)
+        incoming_df['NewAge'] = incoming_df['Age'] * 2
 
-        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        credentials_json = os.environ['GOOGLE_CREDENTIALS_JSON']
-        credentials_info = json.loads(credentials_json)
-        creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
-
-        client = gspread.authorize(creds)
-        sheet = client.open("ADP Test Sheet").sheet1
-
-        new_row_index = len(sheet.get_all_records()) + 1  # +1 to account for header
+        # Find the next row index to update in Google Sheet
+        new_row_index = len(df) + 1  # +1 to account for header
 
         try:
-            new_age_value = df['NewAge'].iloc[-1]
+            # Convert to Python native int
+            new_age_value = int(incoming_df['NewAge'].iloc[-1])
+            # Update the Google Sheet
             sheet.update_cell(new_row_index, 3, new_age_value)
             logging.info('Google Sheet updated successfully')
         except gspread.exceptions.APIError as e:
@@ -48,7 +55,7 @@ def process_data():
         return jsonify({"error": error_message}), 400
 
     # Convert the modified DataFrame back to JSON
-    result = df.to_json(orient='split')
+    result = incoming_df.to_json(orient='split')
     return jsonify(result)
 
 if __name__ == '__main__':
