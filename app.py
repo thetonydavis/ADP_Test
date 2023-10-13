4dc5d98376c5710cc7ac9a52e10000bfbf2c240f
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-13 09:27:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
 import pandas as pd
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def process_data():
-    # Get JSON data from the incoming request
-    data = request.json
-    df = pd.DataFrame(data)
+    data = request.json  # Assuming data is received as JSON
+    try:
+        df = pd.DataFrame(data)
+    except ValueError:
+        # Create an index if data consists of scalar values
+        index = range(len(data)) if data else None
+        df = pd.DataFrame(data, index=index)
     
     # Perform a simple Pandas operation
     df['NewAge'] = df['Age'] * 2
@@ -18,4 +22,4 @@ def process_data():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
