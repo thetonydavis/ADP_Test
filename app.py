79a5c905606e76577bec5ec6ef3369a67b26b9a1
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-13 14:05:17 -0700

    added dynamic rows in sheets

--- app.py
+++ app.py
@@ -17,19 +17,13 @@ def process_data():
     try:
         df = pd.DataFrame(data)
     except ValueError:
-        # Create an index if data consists of scalar values
         index = range(len(data)) if data else None
         df = pd.DataFrame(data, index=index)
 
-    # Check if 'Age' column exists
     if 'Age' in df.columns:
-        # Perform a simple Pandas operation
         df['NewAge'] = df['Age'] * 2
 
-        # Accessing Google Sheets
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-        # Load credentials from environment variable
         credentials_json = os.environ['GOOGLE_CREDENTIALS_JSON']
         credentials_info = json.loads(credentials_json)
         creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
@@ -37,15 +31,19 @@ def process_data():
         client = gspread.authorize(creds)
         sheet = client.open("ADP Test Sheet").sheet1
 
+        # Check the number of rows in the Google Sheet
+        existing_rows = sheet.row_count
+
         try:
-            # Assuming each webhook call corresponds to a single new row being added
-            # Finding the row index for the new data
             new_row_index = len(sheet.get_all_records()) + 2  # +1 to account for header, +1 for new row
 
-            # Updating the 'NewAge' column for the new row
-            new_age_value = df['NewAge'].iloc[-1]  # Getting the last 'NewAge' value from df
-            sheet.update_cell(new_row_index, 3, new_age_value)  # 'NewAge' is the 3rd column
+            # Check if the new_row_index exceeds existing rows
+            if new_row_index > existing_rows:
+                logging.warning(f"New row index {new_row_index} exceeds existing row count {existing_rows}. Please add more rows to the sheet.")
+                return jsonify({"error": "Insufficient rows in Google Sheet"}), 400
 
+            new_age_value = df['NewAge'].iloc[-1]
+            sheet.update_cell(new_row_index, 3, new_age_value)
             logging.info('Google Sheet updated successfully')
 
         except gspread.exceptions.APIError as e:
@@ -57,9 +55,7 @@ def process_data():
         logging.error(error_message)
         return jsonify({"error": error_message}), 400
 
-    # Convert the modified DataFrame back to JSON
     result = df.to_json(orient='split')
-
     return jsonify(result)
 
 if __name__ == '__main__':
