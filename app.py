0679432c60f58be8854e1fb435e60b618df28359
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-13 12:12:50 -0700

    added google api

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import os
 
 app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello, World!"
 
 @app.route('/webhook', methods=['POST'])
 def process_data():
@@ -24,6 +24,19 @@ def process_data():
     else:
         return jsonify({"error": "'Age' column not found in data"}), 400
 
+    # Authenticate with the Google Sheets API
+    credentials_json = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json)
+    client = gspread.authorize(creds)
+
+    # Open the Google Sheet and get the first worksheet
+    sheet = client.open("ADP Test Sheet").sheet1
+
+    # Update the Google Sheet with the modified data
+    # This is a simplified example and may need to be adjusted based on your specific requirements
+    for index, row in df.iterrows():
+        sheet.update('A{}:B{}'.format(index+2, index+2), [[row['Age'], row['NewAge']]])
+
     # Convert the modified DataFrame back to JSON
     result = df.to_json(orient='split')
 
@@ -31,3 +44,4 @@ def process_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
