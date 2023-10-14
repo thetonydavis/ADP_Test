426c4ef3d2c787ed0d26f91cd086dec1e74bf295
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-14 12:21:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,23 @@
-
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, send_file, request, jsonify
 import pandas as pd
-import requests
-import io
-import logging
-import os
+from io import StringIO
+import json
 from datetime import datetime
-from boxsdk import OAuth2, Client
-
-# Initialize the Box SDK
-auth = OAuth2(
-    client_id='dzp83ne72un5revhj27o272sc471v81i',
-    client_secret='u6MZ7XgMPCsKJKMsWgIICwQ9nKTVWAXm',
-    access_token='kWiJ155fJ2fShv9SFnqT0q5nceYD8KSv',  # This should be securely stored and refreshed
-)
-client = Client(auth)
-
-logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route('/rk_summary', methods=['POST'])
 def rk_summary():
-    try:
-        data = request.json
-        app.logger.info(f"Received data: {data}")
+    # Retrieve JSON data from the incoming request
+    data = request.get_json()
+    if not data or 'data' not in data:
+        return jsonify({"error": "Missing data in request"}), 400
 
-        if 'file_url' not in data or not data['file_url']:
-            return jsonify({"error": "Missing or empty 'file_url' in request data"}), 400
+    # Convert received data to DataFrame
+    df = pd.DataFrame(data['data'])
 
-        file_url = data['file_url']
-        
-        app.logger.info(f"Received file_url: {file_url}")
-        
-        response = requests.get(file_url)
-        response.raise_for_status()
-        
-        app.logger.info("Successfully downloaded the file.")
-        
-        df = pd.read_csv(io.StringIO(response.text))
-        app.logger.info("Successfully read the CSV into a DataFrame.")
+    # Tony's DataFrame processing code
+    app.logger.info("Successfully read the CSV into a DataFrame.")
         
         df['Gain/Loss'] = df['Gain/Loss'].str.replace('[$,()]', '', regex=True).astype(float)
         summary_df = df.groupby('Source Number')['Gain/Loss'].sum().reset_index()
@@ -51,23 +29,18 @@ def rk_summary():
         timestamp_str = datetime.now().strftime("%Y%m%d%H%M%S")
         unique_filename = f"summary_{timestamp_str}.csv"
 
-        # Log the unique filename before attempting to upload to Box
-        app.logger.info(f"Attempting to upload file with name: {unique_filename}")
+    # Save DataFrame to a CSV file
+    csv_buffer = StringIO()
+    summary_df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
 
-        csv_file_path = os.path.join(os.getcwd(), unique_filename)
-        summary_df.to_csv(csv_file_path, index=False, encoding='utf-8')
-        
-        # Upload the file to Box
-        folder_id = 'Waivz'
-        folder = client.folder(folder_id=folder_id).get()
-        with open(csv_file_path, 'rb') as f:
-            uploaded_file = folder.upload_stream(f, unique_filename)
-        
-        return send_file(csv_file_path, mimetype='text/csv', as_attachment=True, attachment_filename=unique_filename)
-    
-    except Exception as e:
-        app.logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"error": str(e)}), 500
+    # Use Flask's send_file to return the CSV file
+    return send_file(
+        csv_buffer,
+        as_attachment=True,
+        download_name='summary_' + datetime.now().strftime('%Y%m%d%H%M%S') + '.csv',
+        mimetype='text/csv'
+    )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
