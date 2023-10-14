e87fef8fdc4705488f450538985a22b75c30fe9f
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-14 15:25:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, send_file, request, jsonify
 import pandas as pd
-from io import StringIO
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 @app.route('/rk_summary', methods=['POST'])
 def rk_summary():
     # Retrieve JSON data from the incoming request
-data = request.get_json()
+    data = request.get_json()
     print("===== DEBUGGING START =====")
     print("Received JSON Payload: ", data)
     print("Type of Received JSON Payload: ", type(data))
@@ -28,23 +27,9 @@ data = request.get_json()
     summary_df['Total Gain/Loss'] = summary_df['Total Gain/Loss'].round(2)
     app.logger.info("Successfully summarized the DataFrame.")
 
-    csv_file_path = convert_data_to_csv(response_data['final_report'])
-    return send_file(csv_file_path, mimetype='text/csv').strftime('%Y%m%d%H%M%S') + '.csv',
-            mimetype='text/csv'
-        )
+    # NOTE: You'll need to include the definition of convert_data_to_csv()
+    csv_file_path = "your_generated_file.csv"  # Replace with the actual file path
+    return send_file(csv_file_path, mimetype='text/csv')
 
 if __name__ == '__main__':
     app.run()
-
-def handle_eligibility_status_report():
-    body = request.get_json()
-    print(body)
-    response_data = json.loads(body['eligibility_status_report'])
-    csv_file_path = convert_data_to_csv(response_data['eligibility_status_report'])
-    return send_file(csv_file_path, mimetype='text/csv')
-
-@app.route('/hce_nhce_status_report', methods=['POST'])
-
-
-# Cloned route from raj code.py
-No Flask route found in raj code.py
