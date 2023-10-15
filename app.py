22563ccffd6dce732add2ba163fdcbe0c02743c3
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-14 21:02:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,59 @@
-import json
-
 from flask import Flask, request, jsonify, send_file
+import pandas as pd
+import requests
+import io
+import logging
+import os
 
-from zapier_connection.utils.helper_function import text_to_csv, process_csv_data, check_conditions, convert_data_to_csv
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
+@app.route('/rk_summary', methods=['POST'])
+def rk_summary():
+    try:
+        data = request.json
+        app.logger.info(f"Received data: {data}")
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_home():
-    return 'OK', 200
+        if 'file_url' not in data or not data['file_url']:
+            return jsonify({"error": "Missing or empty 'file_url' in request data"}), 400
 
+        file_url = data['file_url']
+        
+        app.logger.info(f"Received file_url: {file_url}")
+        
+        response = requests.get(file_url)
+        response.raise_for_status()
+        
+        app.logger.info("Successfully downloaded the file.")
+        
+        # Read the CSV content into a DataFrame
+        df = pd.read_csv(io.StringIO(response.text))
+        
+        app.logger.info("Successfully read the CSV into a DataFrame.")
+        
+        # Remove the dollar sign and commas from the 'Gain/Loss' column and convert it to float
+        df['Gain/Loss'] = df['Gain/Loss'].str.replace('[$,()]', '', regex=True).astype(float)
+        
+        # Create sub-totals for the 'Gain/Loss' column
+        summary_df = df.groupby('Source Number')['Gain/Loss'].sum().reset_index()
+        summary_df.columns = ['Source Number', 'Total Gain/Loss']
+        
+        # Round the 'Total Gain/Loss' column to 2 decimal places
+        summary_df['Total Gain/Loss'] = summary_df['Total Gain/Loss'].round(2)
+        
+        app.logger.info("Successfully summarized the DataFrame.")
+        
+        # Write DataFrame to a CSV file
+        csv_file_path = "temporary_summary.csv"  # This will save in the current working directory
+        summary_df.to_csv(csv_file_path, index=False, encoding='utf-8')
+        
+        # Send the file
+        return send_file(csv_file_path, mimetype='text/csv', as_attachment=True, attachment_filename='summary.csv')
+    
+    except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/processData', methods=['POST'])
-def handle_process_data():
-    body = request.get_json()
-    print(body)
-    csv_data = text_to_csv(body['Census_Data'])
-    processed_data = process_csv_data(csv_data, body)
-    response_data = check_conditions(processed_data, body)
-    print(response_data)
-    return jsonify(json.dumps(response_data))
-
-
-@app.route('/dataToCSV', methods=['POST'])
-def handle_data_to_csv():
-    body = request.get_json()
-    response_data = json.loads(body['response_data'])
-    csv_file_path = convert_data_to_csv(response_data['response_data'])
-    return send_file(csv_file_path, mimetype='text/csv')
+if __name__ == '__main__':
+    app.run(debug=True)
