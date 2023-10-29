7f601a28cc0275c932267861c73f8daa7ecd769d
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2023-10-29 11:18:27 -0700

    Update app.py

--- zapier_connection/src/app.py
+++ zapier_connection/src/app.py
@@ -79,3 +79,9 @@ def handle_route_qnce():
     csv_file_path = convert_data_to_csv(individual_qnhce)
     print(csv_file_path)
     return send_file(csv_file_path, mimetype='text/csv')
+
+@app.route('/annotated_progress_bar', methods=['POST'])
+def handle_annotated_progress_bar():
+    body = request.get_json()
+    shorten_url = annotated_progress_bar(body['percentage'], body['label'])
+    return shorten_url
